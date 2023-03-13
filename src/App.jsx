75f6c9a763df6be2a7efedabae9edb54b1efae11
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-03-13 11:39:30 -0300

    React Hooks Felipe Rocha useMemo

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useMemo } from "react";
+
+function App() {
+  const [number, setNumber] = useState(1);
+  const [text, setText] = useState("");
+
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+
+  return (
+    <>
+      <p>{number}</p>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={() => setNumber(2)}>Increment</button>
+      <p>text: {text}</p>
+    </>
+  );
+}
+
+const slowFunction = (num) => {
+  console.log("Slow function is being called!");
+  for (let i = 0; i <= 10000; i++) {
+    return num * 2;
+  }
+};
+
+export default App;
